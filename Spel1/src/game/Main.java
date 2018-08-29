4f44d3c6ccade21bab9119b286a9ba21c75a3ddb
simjoh161 <simjoh161@studentmail.oru.se>
2018-08-29 13:49:14 +0200

    Added the java argument "useSleep" to be able to run the game with the new game loop that uses sleep.

--- Spel1/src/game/Main.java
+++ Spel1/src/game/Main.java
@@ -11,7 +11,12 @@ public class Main {
 	public static void main(String[] args) {
 		ImageIcon img = new ImageIcon("res/images/icon.png");
 		frame = new JFrame("Game 1: Jumper");
-		game = new JumperGame(360, 240, 3);
+		boolean useSleep = false;
+		if(args.length > 0) {
+			if(args[0].equals("useSleep"))
+				useSleep = true;
+		}
+		game = new JumperGame(360, 240, 3, useSleep);
 		frame.add(game);
 		frame.setResizable(false);
 		frame.pack();
